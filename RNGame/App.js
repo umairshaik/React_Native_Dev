6f58667f9712901e5e996b9c19dde6d06176523d
Umair Shaik <umair.shaik@thoughtworks.com>
2024-03-18 14:28:06 +0530

    [Umair][Step-2] set imagebackground  and style

--- RNGame/App.js
+++ RNGame/App.js
@@ -1,8 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <View colors={[]} style={styles.rootContainer}>
+      <ImageBackground
+        style={styles.backgroundContainer}
+        resizeMode="cover"
+        imageStyle={{ opacity: 0.35 }}
+        source={require("./assets/BackgroundImage/background.png")}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    backgroundColor: "#ddb52f",
+  },
+  backgroundContainer: {
+    flex: 1,
+  },
+});
