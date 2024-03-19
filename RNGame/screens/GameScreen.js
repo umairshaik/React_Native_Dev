03abb2d41bb0cefe6090a561c9d49a87b20d0cf7
Umair Shaik <umair.shaik@thoughtworks.com>
2024-03-19 20:03:57 +0530

    [Umair][Step-2] alert and number guess

--- RNGame/screens/GameScreen.js
+++ RNGame/screens/GameScreen.js
@@ -1,7 +1,10 @@
-import { StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import Title from "../components/ui/Title";
 import { useState } from "react";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
+const LOWER = "Lower";
+const GREATER = "Greater";
 
 function generateRandomBetween(min, max, exclude) {
   const randomNum = Math.floor(Math.random() * (max - min)) + min;
@@ -12,14 +15,63 @@ function generateRandomBetween(min, max, exclude) {
   }
 }
 
+let minBoundary = 1;
+let maxBoundary = 100;
+
 function GameScreen({ userNumber }) {
-  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const initialGuess = generateRandomBetween(
+    minBoundary,
+    maxBoundary,
+    userNumber
+  );
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function nextGuessHandler(direction) {
+    if (
+      (direction === LOWER && currentGuess < userNumber) ||
+      (direction === GREATER && currentGuess > userNumber)
+    ) {
+      Alert.alert("You have Lied", "Please dont lie to the system", [
+        { text: "Okay", style: "destructive" },
+      ]);
+      return;
+    }
+
+    if (userNumber == currentGuess) {
+      Alert.alert(
+        "You already won",
+        "The guessed number is the number you have choosen",
+        [{ text: "YES", style: "destructive" }]
+      );
+      return;
+    }
+
+    if (direction == LOWER) {
+      // directions=> 'lower' or 'greater'
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    setCurrentGuess(
+      generateRandomBetween(minBoundary, maxBoundary, currentGuess)
+    );
+  }
 
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
-      <NumberContainer>{initialGuess}</NumberContainer>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <View>
+        <Text>Higher or lower?</Text>
+        <View style={{ flexDirection: "row" }}>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, LOWER)}>
+            -
+          </PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, GREATER)}>
+            +
+          </PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
